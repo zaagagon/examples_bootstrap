ade31d2bc326ca3040f4055814d2c3154365638b
System Administrator <zaagagon@gmail.com>
2022-01-03 21:36:57 -0500

    test js

---
+++ src/bootstrap/ejemplo/test.js
@@ -0,0 +1,11 @@
+// Contents for test.js
+// Represents JSON of the data
+
+var da = `[
+	{ "id": "24323",
+	"name": "Mark Smith",
+	"date": "25/5/2020" },
+	{ "id": "24564",
+	"name": "Caitlin MacDonald",
+	"date": "17/5/2020" }
+]`;
