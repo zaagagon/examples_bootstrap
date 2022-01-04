dd003297d9e5015d49af1faa351cae63bacb7a39
System Administrator <zaagagon@gmail.com>
2022-01-03 21:47:53 -0500

    json en js

--- src/bootstrap/ejemplo/test.js
+++ src/bootstrap/ejemplo/test.js
@@ -1,7 +1,7 @@
 // Contents for test.js
 // Represents JSON of the data
 
-var da = `[
+var datos = `[
 	{ "id": "24323",
 	"name": "Mark Smith",
 	"date": "25/5/2020" },
@@ -12,5 +12,10 @@ var da = `[
         "id":"22223",
         "name":"Al Berckeley",
         "date":"11/12/2020"
+    },
+    {
+        "id":"22223",
+        "name":"Alfonso Cano",
+        "date":"08/12/2020"
     }
 ]`;
