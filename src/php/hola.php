8ffc801dfcf2597c12d0069826a8e889f57e2197
System Administrator <zaagagon@gmail.com>
2022-01-11 00:32:22 -0500

    hoja estilo dentro del html

--- src/php/hola.php
+++ src/php/hola.php
@@ -1,9 +1,14 @@
 <html>
  <head>
   <title>Prueba de PHP</title>
+  <style>
+      /*hola de estilo para nuestro php*/ 
+     h1 {color:red;}
+  p {color:blue;}
+  </style>
  </head>
  <body>
-     <h1>Ejemplo de hola mundo en php</h1>
+     <h1><strong>Ejemplo de hola mundo en php</strong> </h1>
      <!-- php inscrustado en html -->
  <?php echo '<p>Hola Mundo ZAG</p>'; ?>
  <!-- fin php -->
