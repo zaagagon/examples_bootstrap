cd98ef7fdaa55f5669b071605ab2819f91587c60
System Administrator <zaagagon@gmail.com>
2022-01-11 00:36:38 -0500

    php

--- src/php/hola.php
+++ src/php/hola.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>Prueba de PHP</title>
+  <title>Ejemplo PHP</title>
   <style>
       /*hola de estilo para nuestro php*/ 
      h1 {color:red;}
@@ -10,6 +10,7 @@
  <body>
      <h1><strong>Ejemplo de hola mundo en php</strong> </h1>
      <!-- php inscrustado en html -->
+     <!-- estructura php <?php  ?> -->
  <?php echo '<p>Hola Mundo ZAG</p>'; ?>
  <!-- fin php -->
  </body>
