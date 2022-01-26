4ffc16f87880bb9caa86a261d9e9f629a38f6ba9
System Administrator <zaagagon@gmail.com>
2022-01-25 23:24:29 -0500

    code change css

--- src/html/ejemplo5.js
+++ src/html/ejemplo5.js
@@ -1,4 +1,4 @@
-function iniciar(){
+/*function iniciar(){
     var boton=document.getElementById('grabar');
     boton.addEventListener('click', nuevoitem, false);
     window.addEventListener("storage", mostrar, false);
@@ -19,6 +19,44 @@ function iniciar(){
     var clave=localStorage.key(f);
     var valor=localStorage.getItem(clave);
     cajadatos.innerHTML+='<div>'+clave+' - '+valor+'</div>';
+    }
+   }
+   window.addEventListener('load', iniciar, false); */
+
+   function iniciar(){
+    var boton=document.getElementById('grabar');
+    boton.addEventListener('click', nuevoitem, false);
+    mostrar();
+   }
+   function nuevoitem(){
+    var clave=document.getElementById('clave').value;
+    var valor=document.getElementById('texto').value;
+    sessionStorage.setItem(clave,valor);
+    mostrar();
+    document.getElementById('clave').value='';
+    document.getElementById('texto').value='';
+   }
+   function mostrar(){
+    var cajadatos=document.getElementById('cajadatos');
+    cajadatos.innerHTML='<div><button
+    onclick="eliminarTodo()">Eliminar Todo</button></div>';
+    for(var f=0;f<sessionStorage.length;f++){
+    var clave=sessionStorage.key(f);
+    var valor=sessionStorage.getItem(clave);
+    cajadatos.innerHTML+='<div>'+clave+' - '+valor+'<br><button
+    onclick="eliminar(\''+clave+'\')">Eliminar</button></div>';
+    }
+   }
+   function eliminar(clave){
+    if(confirm('Está Seguro?')){
+    sessionStorage.removeItem(clave);
+    mostrar();
+    }
+   }
+   function eliminarTodo(){
+    if(confirm('Está Seguro?')){
+    sessionStorage.clear();
+    mostrar();
     }
    }
    window.addEventListener('load', iniciar, false);
