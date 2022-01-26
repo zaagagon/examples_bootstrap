a9b6be22c2a8a03a71508b5fbb884768b7c4b327
System Administrator <zaagagon@gmail.com>
2022-01-25 23:30:05 -0500

    last change

--- src/html/ejemplo5.js
+++ src/html/ejemplo5.js
@@ -1,4 +1,4 @@
-function iniciar(){
+/*function iniciar(){
     var boton=document.getElementById('grabar');
     boton.addEventListener('click', nuevoitem, false);
     window.addEventListener("storage", mostrar, false);
@@ -21,6 +21,32 @@ function iniciar(){
     cajadatos.innerHTML+='<div>'+clave+' - '+valor+'</div>';
     }
    }
-   window.addEventListener('load', iniciar, false); 
+   window.addEventListener('load', iniciar, false); */
+
+
+   function iniciar(){
+    var boton=document.getElementById('grabar');
+    boton.addEventListener('click', nuevoitem, false);
+    window.addEventListener("storage", mostrar, false);
+    mostrar();
+   }
+   function nuevoitem(){
+    var clave=document.getElementById('clave').value;
+    var valor=document.getElementById('texto').value;
+    localStorage.setItem(clave,valor);
+    mostrar();
+    document.getElementById('clave').value='';
+    document.getElementById('texto').value='';
+   }
+   function mostrar(){
+    var cajadatos=document.getElementById('cajadatos');
+    cajadatos.innerHTML='';
+    for(var f=0;f<localStorage.length;f++){
+    var clave=localStorage.key(f);
+    var valor=localStorage.getItem(clave);
+    cajadatos.innerHTML+='<div>'+clave+' - '+valor+'</div>';
+    }
+   }
+   window.addEventListener('load', iniciar, false);
 
   
